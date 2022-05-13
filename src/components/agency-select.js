9988d58f15f8e3f92f72987124a4e871009ed155
Begerad, Stefan <dialog@swingbe.de>
2022-05-13 13:41:27 +0200

    feat: add bootstrap css to agency select component

---
+++ src/components/agency-select.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dropdown = (props) => (
+    <div className="input-group mb-3">
+        <div className="input-group-prepend">
+            <label className="input-group-text" htmlFor="inputGroupSelectAgency">
+                {props.name}
+            </label>
+        </div>
+        <select
+            className="custom-select"
+            id="inputGroupSelectAgency"
+            name="{props.name}"
+            onChange={props.onChange}
+        >
+            <option selected>Choose...</option>
+            {props.options.map((item, index) => (
+                <option key={index} value={item.agency_id}>
+                    {item.agency_name}
+                </option>
+            ))}
+        </select>
+    </div>
+);
+
+Dropdown.propTypes = {
+    name: PropTypes.string,
+    onChange: PropTypes.func,
+    options: PropTypes.array
+};
+
+export default Dropdown;
--- src/pages/agency-dropdown.js
+++ src/pages/agency-dropdown.js
@@ -1,29 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-export const Dropdown = (props) => (
-    <div className="form-group">
-        <strong>{props.name}</strong>
-        <select
-            className="form-control"
-            name="{props.name}"
-            onChange={props.onChange}
-        >
-            <option defaultValue>Select {props.name}</option>
-            {props.options.map((item, index) => (
-                <option key={index} value={item.agency_id}>
-                    {item.agency_name}
-                </option>
-            ))}
-        </select>
-    </div>
-);
-
-Dropdown.propTypes = {
-    name: PropTypes.string,
-    onChange: PropTypes.func,
-    options: PropTypes.array
-};
+//import Dropdown from '../components/dropdown';
+import Dropdown from '../components/agency-select';
 
 export default class DropdownList extends React.Component {
     constructor () {
