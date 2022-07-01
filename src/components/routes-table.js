10ff908609efb401694a3ed5fb2f0e13562a590f
Begerad, Stefan <dialog@swingbe.de>
2022-07-01 15:23:20 +0200

    feat: add offset, limit and search to routes page

--- src/components/routes-table.js
+++ src/components/routes-table.js
@@ -1,52 +1,46 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import ToggleBtn from './toggle-btn';
-import TableFilterColumn from './table-filter-column';
-import TablePagination from './table-pagination';
-import headings from './routes-table-headings';
-
+import Table from 'react-bootstrap/Table';
+import Entry from './routes-table-entry';
+import Head from './routes-table-head';
 /*the simplest way to define a component is to write a JavaScript function*/
-/*accept a single property object argument*/
-function RoutesTable (props) {
-    //handle state
-    const [toggleBtnState, setToggleBtnState] = useState(false);
-    /*create headings*/
-    const columns = React.useMemo(() => headings, []);
-
-    /*create data*/
-    /*updating hooks is done by the dependency array*/
-    /*define variables that 'trigger' the change on hooks*/
-    const data = React.useMemo(() => props.entries, [props.entries]);
-
-    const toggle = () => {
-        setToggleBtnState((toggleBtnState) => !toggleBtnState);
+/*destructure props object*/
+function RoutesTable ({ aryData }) {
+    const [searchField, setSearchField] = useState('');
+    const handleAryData = () => {
+        if (aryData.length > 0) {
+            //iterate over array
+            return aryData.map((item, index) => {
+                //console.log('aryData index: ' + index);
+                return (
+                    <Entry
+			routeId={item.route_id}
+			agencyId={item.agency_id}
+			routeShortName={item.route_short_name}
+			routeLongName={item.route_long_name}
+			routeType={item.route_type}
+			routeColor={item.route_color}
+			routeTextColor={item.route_text_color}
+			routeDesc={item.route_desc}
+                        key={index}
+                    />
+                );
+            });
+        }
     };
-    const toggleBtn = (
-        <ToggleBtn
-            btnState={toggleBtnState}
-            btnFalse="paging"
-            btnTrue="all"
-            descFalse=": click button to present all routes at once"
-            descTrue=": click button to present routes in pages"
-            btnOnClick={toggle}
-        />
-    );
-
+    /*return a React element*/
     return (
         <>
-            {toggleBtn}
-            {props.entries &&
-        (toggleBtnState ? (
-            <TableFilterColumn columns={columns} data={data} />
-        ) : (
-            <TablePagination columns={columns} data={data} />
-        ))}
+            <Table striped bordered hover size="sm" variant="dark" responsive>
+                <thead>
+                    <Head />
+                </thead>
+                <tbody>{handleAryData()}</tbody>
+            </Table>
         </>
     );
 }
-
 RoutesTable.propTypes = {
-    entries: PropTypes.array
+    aryData: PropTypes.array
 };
-
 export default RoutesTable;
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,39 +1,91 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Select from '../components/select';
+import {selectOptions} from '../utils/select-options';
 import RoutesTable from '../components/routes-table';
-
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import InputSearch from '../components/input-search';
 const Routes = () => {
-    const [data, setData] = useState([]);
-
-    const getData = async () => {
+    /*store and initialise data in function component state*/
+    const [oset, setOset] = useState(1);
+    const [limit, setLimit] = useState(selectOptions[2]);
+    const [ary, setAry] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    const filteredAry = ary.filter((item, index) => {
+        return (
+            item.route_id.toLowerCase().includes(searchField.toLowerCase()) ||
+            item.agency_id.toLowerCase().includes(searchField.toLowerCase()) ||
+            item.route_short_name.toLowerCase().includes(searchField.toLowerCase()) ||
+            item.route_long_name.toLowerCase().includes(searchField.toLowerCase()) ||
+            item.route_type.toString().includes(searchField.toLowerCase()) ||
+            item.route_color.toLowerCase().includes(searchField.toLowerCase()) ||
+            item.route_text_color.toLowerCase().includes(searchField.toLowerCase()) ||
+            item.route_desc.toLowerCase().includes(searchField.toLowerCase())
+        );
+    });
+    /*fetch ary in a JavaScript function*/
+    const fetch = async () => {
         try {
             /*TODO make route available using config*/
             /*TODO handle errors: https://www.valentinog.com/blog/await-react/*/
-            const res = await axios.get(
-                'https://v1gtfs.vbn.api.swingbe.de/routes-all'
-            );
+            const address = `https://v1gtfs.vbn.api.swingbe.de/routes-oset-limit?oset=${oset}&limit=${limit}`;
+            const res = await axios.get(address);
 
             /*set state*/
-            setData(res.data);
+            setAry(res.data);
         } catch (err) {
             console.log('err.message: ' + err.message);
         }
     };
 
     /*this hook is run after a DOM update. Changing state migh result in an infinite loop*/
-    useEffect(() => {
-    /*effect goes here*/
-
         /*hook need to be placed in body of the function component in which it is used*/
-        getData();
-
-    /*use an empty dependency array to ensure the hook is running only once*/
-    /*TODO study dependency array: https://reactjs.org/docs/hooks-effect.html*/
-    }, []);
-
+    useEffect(() => {
+	/*effect goes here*/
+        fetch();
+	/*use an empty dependency array to ensure the hook is running only once*/
+	/*TODO study dependency array: https://reactjs.org/docs/hooks-effect.html*/
+    }, [oset, limit]);
+    const handleClickPrev = () => {
+        setOset((oset) => (oset > 1 ? --oset : oset));
+    };
+    const handleClickNext = () => {
+        setOset((oset) => ++oset);
+    };
+    const handleChangeLimit = (event) => {
+        setLimit((limit) => event.target.value);
+    };
+    const handleSearch = (e) => {
+        setSearchField(e.target.value);
+    };
     return (
         <>
-            <RoutesTable entries={data} />
+            <Stack direction="horizontal" gap={1} className="m-1">
+                <Button variant="secondary" onClick={handleClickPrev} autoFocus>
+          prev
+                </Button>
+                <Button variant="secondary" onClick={handleClickNext}>
+          next
+                </Button>
+		<Select
+		    defaultValue={selectOptions[2]}
+		    id="shapesLimit"
+		    name="shapesLimit"
+		    onChange={handleChangeLimit}
+		    options={selectOptions}
+		/>
+		<InputSearch
+		    id="shapesSearch"
+		    name="shapesSearch"
+                    onChange={handleSearch}
+                    placeholder="Search table globally"
+                    type="search"
+                    title="Enter search value"
+		    value={searchField}
+		/>
+            </Stack>
+	    <RoutesTable aryData={filteredAry} />
         </>
     );
 };
