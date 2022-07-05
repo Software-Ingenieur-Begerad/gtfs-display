a957846a13d2a6edd8fe7c22ee81e5e8e3d63c5f
Begerad, Stefan <dialog@swingbe.de>
2022-07-05 11:45:25 +0200

    chore: adjust API value

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 export default {
-    API: 'https://v1gtfs.vbn.api.swingbe.de/',
+    API: 'https://v1gtfs.delfi.api.swingbe.de/',
     GTFS_VALIDATOR_REPORT: 'https://www.v1gtfs.delfi.api.swingbe.de/gtfs-validator/report.html',
 };
