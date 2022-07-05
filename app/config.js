dfc9b471f7b8ad88515513447e6b86dc9bf81341
Begerad, Stefan <dialog@swingbe.de>
2022-07-05 11:36:18 +0200

    chore: adjust GTFS_VALIDATOR_REPORT value

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 export default {
     API: 'https://v1gtfs.vbn.api.swingbe.de/',
-    GTFS_VALIDATOR_REPORT: 'https://www.v1gtfs.vbn.api.swingbe.de/gtfs-validator/report.html',
+    GTFS_VALIDATOR_REPORT: 'https://www.v1gtfs.delfi.api.swingbe.de/gtfs-validator/report.html',
 };
